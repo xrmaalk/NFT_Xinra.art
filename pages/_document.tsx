d4d9df8f73a7081ea3de071476380d14a0abe3bf
MKXinrare <33601035+xrmaalk@users.noreply.github.com>
2022-03-12 12:18:13 -0700

    Google Font Modification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
-import Configuration from '~/utils/configuration'
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import Configuration from "~/utils/configuration";
 
 class MyDocument extends Document {
   render() {
@@ -11,10 +11,20 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
-        
-          <link href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@342;458&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Creepster&display=swap" rel="stylesheet" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin=""
+          />
+
+          <link
+            href="https://fonts.googleapis.com/css2?family=Readex+Pro:wght@342;458&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap"
+            rel="stylesheet"
+          />
           {/* <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet" /> */}
         </Head>
         <body>
@@ -22,8 +32,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
