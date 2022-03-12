93ba9f1b7afdff5c04eb21d77c9b4e328cd7e371
MKXinrare <33601035+xrmaalk@users.noreply.github.com>
2022-03-12 09:45:05 -0700

    About Home Screen Descirption update

--- components/stories/About.tsx
+++ components/stories/About.tsx
@@ -1,7 +1,7 @@
-import { FunctionComponent } from 'react'
-import AboutContainer from '~/components/elements/AboutContainer'
-import Configuration from '~/utils/configuration'
-import type { Item } from '~/pages/api/items'
+import { FunctionComponent } from "react";
+import AboutContainer from "~/components/elements/AboutContainer";
+import Configuration from "~/utils/configuration";
+import type { Item } from "~/pages/api/items";
 
 const About: FunctionComponent = () => {
   return (
@@ -9,28 +9,39 @@ const About: FunctionComponent = () => {
       id="about"
       beforeTitle="Welcome to"
       title={Configuration.title}
-      subtitle={(
+      subtitle={
         <div>
           <p>
-            In one night seemingly creepy but very cute monsters filled the streets of cities all over the world. 
-            Every creature needs a home, so an unknown organization called Monster Shelter set itself the goal of putting these small critters in good hands...
+            The Making of Series, introduces 3D Collectible Model ownership to
+            the blockchain. Each NFT is redeemable for a unique 3D Printed
+            Model.
           </p>
           <p>
-            Monster Shelter is a generative collection of animated monsters NFT with a variety of colorful backgrounds, 
-            inspired by horror culture. Each monster has up to 10 traits on itself and its own color palette, 
-            creating an individual appearance and character of the animal.
+            Collect Xinra Inc's debute collection TMOS: DBZ. This Ultra-rare
+            collection consists of four uniquly hand-painted SuperSayinWarriors.
+            Become one of four owners in the series. Hodling a TMOS NFT grants
+            the owners perks in product discounts in any of Xinra<sup>Inc</sup>
+            's Marketplaces.
           </p>
           <p>
-            After the launch monsters will become playable characters in our Tamagotchi P2E game.
+            After the launch the team will split into a fresh NFT marketplace
+            development plus further development of our P2E game RodNinjas..
           </p>
         </div>
-      )}
+      }
     >
       <div>
-        <iframe style={{ aspectRatio: '560 / 315', width: '100%' }} src="https://www.youtube-nocookie.com/embed/ysuZ_SW2bK0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
+        <iframe
+          style={{ aspectRatio: "560 / 315", width: "100%" }}
+          src="https://www.youtube-nocookie.com/embed/ysuZ_SW2bK0"
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
       </div>
     </AboutContainer>
-  )
-}
+  );
+};
 
-export default About
+export default About;
